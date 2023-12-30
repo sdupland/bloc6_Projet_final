d04b5c5388c2ed147c2b631c6dd7e700f3e84d60
sdupland <133764524+sdupland@users.noreply.github.com>
2023-12-30 19:34:16 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,9 @@ email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
-[theme]\n\
-base="dark"\n\
-primaryColor="#4be8e0"\n\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+echo "\
 " > ~/.streamlit/config.toml
